7e6fd7f6a24a2a25e53c81f4218c423c8c23b818
GeoffMcLennan <geoff.d.mclennan@gmail.com>
2016-09-17 15:38:46 -0700

    Finished building testing platform

---
+++ scripts/fetchProduct.js
@@ -0,0 +1,48 @@
+/* Fetches JSON object via HTTP */
+function fetchProduct() {
+    var sku = $("input#skuNumber").val();
+
+    var params = {
+        // Request paramters
+    };
+
+    $.ajax({
+        url: "https://api.builddirect.io/products/" + sku + "?" + $.param(params),
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","d83e94bef8644cec98cd9b9662f534c8");
+        },
+        type: "GET",
+        // Request body
+        data: "{body}",
+    })
+    .done(function(data) {
+        var weightPerBox = data.data.sellUnitDetails.weight;
+        getParams(weightPerBox);
+    })
+    .fail(function() {
+        alert("error");
+    });
+}
+
+/* Calculates all necessary parameters for rate calculation. */
+function getParams(weightPerBox) {
+    boxCount = $("input#boxCount").val();
+    palletCount = $("input#palletCount").val();
+    delArea = $("select#location").val();
+    distance = $("input#distance").val();
+    numStairs = $("input#numStairs").val();
+    var chokePoint = $("input#chokePoint").val();
+
+    totalWeight = weightPerBox * boxCount;
+
+    if ((chokePoint < 48) || (numStairs > 0)){
+        unpack = true;
+    } else {
+        unpack = false;
+    }
+
+    $("p#skuOutput").append(totalWeight + ", " + numStairs + ", " + distance + ", "
+        + palletCount + ", " + boxCount + ", " + unpack + ", " + delArea);
+
+}
