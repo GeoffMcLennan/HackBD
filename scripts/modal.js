04b810c52666b85fc04b4849e03164d59948874e
Sheldon Lynn <sheldonlynn@outlook.com>
2016-09-18 07:44:36 -0700

    created toggle function

--- scripts/modal.js
+++ scripts/modal.js
@@ -12,16 +12,16 @@
       modal.style.display = "block";
     }
 
-	
+
     // Get the button that opens the modal
     var imgMap = document.getElementById("imgMap");
-	
+
     // When the user clicks the button, open the modal
-    imgMap.onclick = function() {
-	  window.scroll(0,0);
+    imgMap.onclick = function () {
+      window.scroll(0, 0);
       modal.style.display = "block";
-    }	
-	
+    }
+
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
       modal.style.display = "none";
@@ -36,78 +36,25 @@
 
     var currentIndex = 0;
 
-    $('#next-button').on('click', function () {
-      if (currentIndex == 0) {
-        $('#content0').css('display', 'none');
-        $('#content1').css('display', 'flex');
-        currentIndex++;
-      } else if (currentIndex == 1) {
-        $('#content1').css('display', 'none');
-        $('#content2').css('display', 'flex');
-        currentIndex++;
-      } else if (currentIndex == 2) {
-        $('#content2').css('display', 'none');
-        $('#content3').css('display', 'flex');
-        currentIndex++;
-      } else if (currentIndex == 3) {
-        $('#content3').css('display', 'none');
-        $('#content4').css('display', 'flex');
-        currentIndex++;
-      } else if (currentIndex == 4) {
-        $('#content4').css('display', 'none');
-        $('#content5').css('display', 'flex');
-        currentIndex++;
-      } else if (currentIndex == 5) {
-        $('#content5').css('display', 'none');
-        $('#content6').css('display', 'flex');
-        currentIndex++;
-        fetchProduct();
-		document.getElementById("next-button").textContent="Finish";
-      } else if (currentIndex == 6) {
-        $('#content0').css('display', 'none');
-        $('#content1').css('display', 'none');
-        $('#content2').css('display', 'none');
-        $('#content3').css('display', 'none');
-        $('#content4').css('display', 'none');
-        $('#content5').css('display', 'none');
-        modal.style.display = "none";
+    function toggleVisibility(nextIndex, backForth) {
+      var toggleOn = $('#content' + nextIndex);
+      if (backForth > 0) {
+        var offTarget = nextIndex - 1;
+      } else if (backForth < 0) {
+        var offTarget = nextIndex + 1;
       }
+      var toggleOff = $('#content' + offTarget);
+
+      $(toggleOn).css('display', 'flex');
+      $(toggleOff).css('display', 'none');
+    }
+
+    $('#next-button').on('click', function () {
+      currentIndex++;
+      toggleVisibility(currentIndex, 1);
     });
 
     $('#back-button').on('click', function () {
-      if (currentIndex == 0) {
-        $('#content0').css('display', 'flex');
-        $('#content1').css('display', 'none');
-        $('#content2').css('display', 'none');
-        $('#content3').css('display', 'none');
-        $('#content4').css('display', 'none');
-        $('#content5').css('display', 'none');
-        $('#content6').css('display', 'none');
-		modal.style.display = "none";
-      } else if (currentIndex == 1) {  
-        $('#content0').css('display', 'flex');
-        $('#content1').css('display', 'none');
-        currentIndex--;
-      } else if (currentIndex == 2) {
-        $('#content1').css('display', 'flex');
-        $('#content2').css('display', 'none');
-        currentIndex--;
-      } else if (currentIndex == 3) {
-        $('#content2').css('display', 'flex');
-        $('#content3').css('display', 'none');
-        currentIndex--;
-      } else if (currentIndex == 4) {
-        $('#content3').css('display', 'flex');
-        $('#content4').css('display', 'none');
-        currentIndex--;
-      } else if (currentIndex == 5) {
-        $('#content4').css('display', 'flex');
-        $('#content5').css('display', 'none');
-        currentIndex--;
-      } else if (currentIndex == 6) {
-        $('#content5').css('display', 'flex');
-        $('#content6').css('display', 'none');
-		document.getElementById("next-button").textContent="Next >";   
-        currentIndex--;
-      }
+      currentIndex--;
+      toggleVisibility(currentIndex, -1);
     });
