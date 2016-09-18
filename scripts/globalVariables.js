b38e167b7e99f138f8e62bd7e8a0f702f3aa4021
Sheldon Lynn <sheldonlynn@outlook.com>
2016-09-18 04:56:17 -0700

    supplier portal created

--- scripts/globalVariables.js
+++ scripts/globalVariables.js
@@ -10,3 +10,4 @@ var selectArea = [["yard", 0], ["house", 100], ["apartment", 200]];
 var selectDistance = [["0 - 15ft", 0], ["15 - 30ft", 30], ["30 - 50ft", 70], ["50+ft", 120]];
 var selectStairs = [["Flat Surface", 0], ["One Storey", 75], ["Two Stories", 150]];
 var selectChoke = [["Less than 48", 100], ["Greater than 48", 0]];
+var multObj1 = [];
